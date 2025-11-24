716dd5af7f8216ebdfc276bd097ce884189ef2db
Sharhad Bashar <sharhad.bashar@uwaterloo.ca>
2025-11-24 13:35:31 -0500

    removed fitz

--- app/api/v0/endpoints/reports.py
+++ app/api/v0/endpoints/reports.py
@@ -5,7 +5,7 @@ from app.crud import reports, tasks
 from app.schema.tasks import Tasks, Task_Type, Status
 from app.schema.properties import Reports, Reports_Simple
 from app.core.common.aws_operations import AWS_Operations
-from app.core.property_report_extract.issue_extract import IssueExtract
+# from app.core.property_report_extract.issue_extract import IssueExtract
 
 router = APIRouter()
 
@@ -29,41 +29,41 @@ def get_listing_reports(listing_id: int):
 async def create(report: Reports):
     return await reports.create(report)
 
-@router.post('/extract/issues')
-async def extract_issues(
-    background_tasks: BackgroundTasks,
-    user_id: int = Form(...),
-    listing_id: int = Form(...), 
-    name: str = Form(...),
-    property_report: UploadFile = File(...)
-):
-    if not property_report.filename.endswith('.pdf'):
-        raise HTTPException(status_code = 400, detail = 'Only PDF files are allowed')
+# @router.post('/extract/issues')
+# async def extract_issues(
+#     background_tasks: BackgroundTasks,
+#     user_id: int = Form(...),
+#     listing_id: int = Form(...), 
+#     name: str = Form(...),
+#     property_report: UploadFile = File(...)
+# ):
+#     if not property_report.filename.endswith('.pdf'):
+#         raise HTTPException(status_code = 400, detail = 'Only PDF files are allowed')
     
-    property_report_content = await property_report.read()
-    await property_report.seek(0)
+#     property_report_content = await property_report.read()
+#     await property_report.seek(0)
 
-    # aws_operations = AWS_Operations()
-    # aws_link = await aws_operations.upload_file(user_id, listing_id, name, property_report)
-    aws_link = 'temp:link:aws:s3'
-    report = await reports.create(Reports(
-        user_id = user_id,
-        listing_id = listing_id,
-        aws_link = aws_link,
-        name = name
-    ))
-    task_id = await tasks.create(Tasks(
-        report_id = report['id'], 
-        task_type = Task_Type.EXTRACT_ISSUES.value, 
-        status = Status.PENDING.value
-    ))
-    extract_issues = IssueExtract(report['id'], property_report.filename, property_report_content, task_id['id'])
-    background_tasks.add_task(extract_issues.run)
-    return {
-        'report_id': report['id'], 
-        'task_id': task_id['id'], 
-        'aws_link': aws_link
-    }
+#     # aws_operations = AWS_Operations()
+#     # aws_link = await aws_operations.upload_file(user_id, listing_id, name, property_report)
+#     aws_link = 'temp:link:aws:s3'
+#     report = await reports.create(Reports(
+#         user_id = user_id,
+#         listing_id = listing_id,
+#         aws_link = aws_link,
+#         name = name
+#     ))
+#     task_id = await tasks.create(Tasks(
+#         report_id = report['id'], 
+#         task_type = Task_Type.EXTRACT_ISSUES.value, 
+#         status = Status.PENDING.value
+#     ))
+#     extract_issues = IssueExtract(report['id'], property_report.filename, property_report_content, task_id['id'])
+#     background_tasks.add_task(extract_issues.run)
+#     return {
+#         'report_id': report['id'], 
+#         'task_id': task_id['id'], 
+#         'aws_link': aws_link
+#     }
 
 
 @router.put('/{id}')
