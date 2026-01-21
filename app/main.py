1e578922faad6050e278ed358adf1f63b8741438
Sharhad Bashar <sharhad.bashar@uwaterloo.ca>
2026-01-21 13:53:12 -0500

    added cors

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.runner import api_router
 from app.core.config import settings
@@ -6,6 +7,15 @@ from app.core.config import settings
 app = FastAPI(
     title = settings.PROJECT_NAME
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
 @app.get('/')
 def get_all():
     return {'message': 'Go to /api/<VERSION>/ to see the API endpoints'}
